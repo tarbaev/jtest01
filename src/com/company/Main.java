90ee6500213600488694a5c474762039598f4209
Oleg <tarbaev.oleg@gmail.com>
2021-06-03 16:18:32 +0300

    01

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String msg;
+        msg = "Hello World";
+        System.out.println(msg);
+
+        msg = "Привет Мир";
+        System.out.println(msg);
+    }
+}
